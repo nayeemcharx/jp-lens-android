ede28d1ef7d88ce1302e35dcbd4fedf6255936e3
nayeemcharx <n.nayeem.rm@gmail.com>
2026-07-11 04:12:47 +0600

    misc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.nayeemcharx.jplens"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
